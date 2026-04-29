53e64dd273506b6aa47aa8905dee866395980710
Devaaldo <akbarprdn2512@gmail.com>
2026-04-29 12:02:53 +0700

    fix: remove conflicting start_run

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,7 +8,7 @@ TRAIN_PATH = 'diabetesdataset_preprocessing/diabetes_train.csv'
 TEST_PATH  = 'diabetesdataset_preprocessing/diabetes_test.csv'
 TARGET_COL = 'Outcome'
 
-mlflow.set_experiment('diabetes-classification')
+mlflow.sklearn.autolog()
 
 train_df = pd.read_csv(TRAIN_PATH)
 test_df  = pd.read_csv(TEST_PATH)
@@ -18,11 +18,8 @@ y_train = train_df[TARGET_COL]
 X_test  = test_df.drop(TARGET_COL, axis=1)
 y_test  = test_df[TARGET_COL]
 
-with mlflow.start_run(run_name='RandomForest-CI'):
-    mlflow.sklearn.autolog()
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    print(f'Accuracy: {accuracy_score(y_test, y_pred):.4f}')
+y_pred = model.predict(X_test)
+print(f'Accuracy: {accuracy_score(y_test, y_pred):.4f}')
